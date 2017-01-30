48ab1be278cd78ca3e1f1b3f47fca14961155edb
Richard Strauss <rstrauss91@gmail.com>
2017-01-30 14:46:08 -0500

    Create before_action requirementsin controller for kitten(s)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
 
+  before_action :set_kitten_url, only: [:kitten, :kittens]
+
 def welcome
 end
 
@@ -10,8 +12,16 @@ def contest
 end
 
 def kitten
-  requested_size = params[:size]
-  @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
 end
 
+def kittens
+end
+
+end
+
+
+
+def set_kitten_url
+  requested_size = params[:size]
+  @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
 end
