422a72329abbd6c01ce912979eecc07daadcd49e
Richard Strauss <rstrauss91@gmail.com>
2017-01-30 14:59:20 -0500

    create secret url route

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,7 +10,7 @@ end
 
 def contest
   flash[:notice] = "Sorry, the contest has ended"
-  # redirect_to '/welcome'
+  redirect_to '/welcome'
 end
 
 def kitten
@@ -19,11 +19,17 @@ end
 def kittens
 end
 
+def secrets
+  if params[:magic_word] == 'ricardo'
+  else
+    flash[:alert] = "Sorry, you're not authorized to see that page!"
+    redirect_to '/welcome'
+  end
 end
-
-
 
 def set_kitten_url
   requested_size = params[:size]
   @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
+end
+
 end
